337d3beb9f5a9e41eb89addec0457b16afd32faf
Om Sharma <omsharma.83173@gmail.com>
2024-12-03 03:36:00 +0530

    for-netifly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel/serverless';
+
+
+import netlify from '@astrojs/netlify';
+
+
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify(),
 });
